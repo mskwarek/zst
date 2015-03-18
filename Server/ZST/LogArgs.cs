c5336f87a479e3d2af4ed8baf36280eed92a7818
unknown <mskwarek@stud.elka.pw.edu.pl>
2015-03-18 22:17:33 +0100

    logi

---
+++ Server/ZST/LogArgs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZST
+{
+    class LogArgs : EventArgs
+    {
+        private string[] message;
+
+        public LogArgs(string[] message)
+        {
+            this.message = message;
+        }
+
+        // This is a straightforward implementation for 
+        // declaring a public field
+        public string[] Message
+        {
+            get
+            {
+                return message;
+            }
+        }
+    }
+}
